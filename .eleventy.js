b7f2fc19d2e8d798eaccb181e41e18b503943d42
Toby Avila <88242860+Vaserium@users.noreply.github.com>
2024-01-25 08:53:20 -0500

    11tyfied-the-static-pages

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  //addPassthroughCopy is copy pasting everything other than the .html files to the public folder
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+  // DateTime.fromJSDate(dateObj) will be converted to a local string using the DateTime.DATE_MED which is all coming from luxon
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  return {
+    dir: {
+      input: "src", //All the files I will be working from will be in src folder
+      output: "public", //Public folder
+    },
+  };
+};

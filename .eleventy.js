fbcecc0edcc1a89dfa57bdf568afe2e33492a793
Toby Avila <88242860+Vaserium@users.noreply.github.com>
2024-01-25 09:44:15 -0500

    fixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
   //addPassthroughCopy is copy pasting everything other than the .html files to the public folder
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
   // DateTime.fromJSDate(dateObj) will be converted to a local string using the DateTime.DATE_MED which is all coming from luxon
   eleventyConfig.addFilter("postDate", (dateObj) => {
